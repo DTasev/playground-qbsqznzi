d2987b0f8d0de52c5bf135a8e55ed36d1c3c75e6
Dimitar Tasev <dimtasev@gmail.com>
2018-05-10 16:07:19 +0100

    adw

--- python-project/string_asm.py
+++ python-project/string_asm.py
@@ -31,7 +31,7 @@ fn = mkfunction("""
 fn.restype = ctypes.c_uint32
 
 
-with open('asm_content', 'rb') as f:
+with open('asm_content', 'r') as f:
     asm_lines = "".join(f.readlines())
 fn2 = mkfunction(asm_lines)
 fn2.restype = ctypes.c_uint32
