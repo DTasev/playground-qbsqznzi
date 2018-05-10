3b0a40d7b5c425539ad7a3722809de6bf5065c4e
Dimitar Tasev <dimtasev@gmail.com>
2018-05-10 15:25:01 +0100

    added pycca example

--- python-project/universe.py
+++ python-project/universe.py
@@ -1,5 +1,24 @@
-def count_all_stars(galaxies):
-    total_stars = 0
-    for stars in galaxies:
-        total_stars = stars  # fix me!
-    return total_stars
+import ctypes
+import struct
+import time
+import math
+
+from pycca.asm import *
+
+
+print("""
+   Example 1: Return a value from call
+------------------------------------------------------
+""")
+
+# To return a value from a function, just put it in eax or rax:
+fn = mkfunction([
+    mov(eax, 0xdeadbeef),
+    ret()
+])
+
+# Tell ctypes how to interpret the return value
+fn.restype = ctypes.c_uint32
+
+# Call! Hopefully we get 0xdeadbeef back.
+print("Return: 0x%x" % fn())
