6fc4e5e3fcbb58d7182b7be56bbd1c29bb1cef3e
Dimitar Tasev <dimtasev@gmail.com>
2018-05-10 15:42:18 +0100

    change autofold

--- python-project/universe.py
+++ python-project/universe.py
@@ -12,12 +12,11 @@ print("""
 ------------------------------------------------------
 """)
 
+# // }
 # To return a value from a function, just put it in eax or rax:
 fn = mkfunction([
-    # // }
     mov(eax, 0xdeadbeef),
     ret()
-    # { autofold
 ])
 
 # Tell ctypes how to interpret the return value
@@ -25,4 +24,3 @@ fn.restype = ctypes.c_uint32
 
 # Call! Hopefully we get 0xdeadbeef back.
 print("Return: 0x%x" % fn())
-# }
